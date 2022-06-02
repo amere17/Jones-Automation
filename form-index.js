b246f59507c73f4d4e818bc65480ffba1a4bba74
Your Name <you@example.com>
2022-06-02 10:22:31 +0300

    Test

--- form-index.js
+++ form-index.js
@@ -1,27 +1,28 @@
 const puppeteer = require('puppeteer');
 // Data Arrays
-// there is an option to send the data as an arguments
-//Bugs: Name must has first and second name & the phone field must not acceptting letters!!
+// There is an option to send the data as an arguments
+// Bugs: Name must has first and second name & the phone field must not acceptting letters!!
 const dataMap = new Map([["#name","amere"],["#email","aq@gmail.com"],["#phone","+972509376658"],["#company","Amere-Tech"]]);
 
 (async () => {
-  // launch the browser and the website
+  // Launch the browser and the website
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto('http://contractorsinsurancereview.com/ExampleForm/')
-  // type the values in the input field
+  // Type the values in the input field
   for(const [key,value] of dataMap){
     await page.type(key,value);
   }
   await page.select("#employees","51-500")
-  // make a screenshot and save it then close the browser 
+  // Make a screenshot and save it then close the browser 
   await page.screenshot({ path: 'img1.png', fullPage: 'true' });
-  await page.click('button.primary.button')// click the submission button
+  // Click the "Request a Call Back" button
+  await page.click('button.primary.button');
   await page.screenshot({ path: 'img2.png', fullPage: 'true' });
   
   const tuPage = await page.$x("//h1[contains(text(), 'Thank You')]");
   
-  // check if there is a thank you text in the page
+  // Check if there is a thank you text in the page
   if(tuPage.length > 0)
     console.log("Thank you");
   else
